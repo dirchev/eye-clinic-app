8d137f08e8b3c79e28aa2a37599a322e2f89cf82
Dimitar Mirchev <dirchev@camplight.net>
2016-03-08 23:49:11 +0000

    Add search field to PatientList and make it work

--- src/eyeclinic/Pages/App/PatientsList/Controller.java
+++ src/eyeclinic/Pages/App/PatientsList/Controller.java
@@ -11,9 +11,12 @@ import eyeclinic.Patient;
 import eyeclinic.Stores.ModalsStore;
 import eyeclinic.Stores.PatientsStore;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 /**
@@ -24,6 +27,7 @@ import javafx.scene.layout.VBox;
 public class Controller implements Initializable {
 
     public VBox container = new VBox();
+    public TextField patientSearchField;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -31,9 +35,23 @@ public class Controller implements Initializable {
     }
     
     public void updatePatientsList () {
+        this.updatePatientsListContents(PatientsStore.getPatients());
+    }
+    
+    public void onSearchFieldUpdate () {
+        String searchText = patientSearchField.getText().trim();
+        this.updatePatientsListContents(PatientsStore.searchByName(searchText));
+    }
+    
+    private void updatePatientsListContents (ArrayList<Patient> patients) {
         container.getChildren().clear();
-        for (Patient patient : PatientsStore.getPatients()) {
-            container.getChildren().add(new PatientItem(patient));
+        if (patients.size() == 0) {
+            Label message = new Label("No patients found...");
+            container.getChildren().add(message); 
+        } else {
+            for (Patient patient : patients) {
+                container.getChildren().add(new PatientItem(patient));
+            }
         }
     }
     
