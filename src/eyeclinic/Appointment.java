41d60a8bfe98a58133f7a3b3053a14c5d6835b2f
Dimitar Mirchev <dirchev@camplight.net>
2016-03-12 17:28:53 +0000

    implement appointment class

--- src/eyeclinic/Appointment.java
+++ src/eyeclinic/Appointment.java
@@ -5,10 +5,55 @@
  */
 package eyeclinic;
 
+import java.util.Date;
+
 /**
  *
  * @author dirchev
  */
 public class Appointment {
+    private Date startDate;
+    private Date endDate;
+    private Patient patient;
+    private Treatment treatment;
+    private Staff optician;
+    
+    public Appointment (Treatment treatment, Date startDate, Date endDate) {
+        this.patient = treatment.getPatient();
+        this.treatment = treatment;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }   
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public Treatment getTreatment() {
+        return treatment;
+    }
+    public Staff getOptician() {
+        return optician;
+    }
+
+    public void setOptician (Staff optician) {
+        this.optician = optician;
+    }
     
 }
