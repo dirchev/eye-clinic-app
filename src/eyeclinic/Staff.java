224428e9deb15448ee9e1d026f2bfb93e742c410
Dimitar Mirchev <dirchev@camplight.net>
2016-03-19 17:15:03 +0000

    Add javadoc for Staff class

--- src/eyeclinic/Staff.java
+++ src/eyeclinic/Staff.java
@@ -1,21 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package eyeclinic;
 
 /**
- *
+ * Implementation of Staff interface.
+ * <p>
+ * Holds all fields, needed for a eye clinic's staff member.
  * @author dirchev
  */
 public class Staff {
-    private String id;
     private String username;
     private String password;
     private String fullName;
     private String role; // "receptionist" or "optician"
     
+    /**
+     * Creates new receptionist.
+     * @param username username of the staff member
+     * @param password the password of the staff member
+     * @param fullName the full name of the staff member
+     */
     public Staff (String username, String password, String fullName) {
         this.username = username;
         this.password = password;
@@ -23,6 +25,13 @@ public class Staff {
         this.role = "receptionist";
     }
     
+    /**
+     * Creates new staff member.
+     * @param username username of the staff member
+     * @param password the password of the staff member
+     * @param fullName the full name of the staff member
+     * @param role the staff member's role. Can be "receptionist" or "optician"
+     */
     public Staff (String username, String password, String fullName, String role) {
         this.username = username;
         this.password = password;
@@ -30,10 +39,20 @@ public class Staff {
         this.role = role;
     }
     
+    /**
+     * Checks if credentials match the member's credentials
+     * @param username username to check
+     * @param password password to check
+     * @return true if the credentials match or false if the don't
+     */
     public Boolean checkCredentials (String username, String password) {
         return username.equals(this.username) && password.equals(this.password);
     }
     
+    /**
+     * Returns the full name of the staff member
+     * @return full name of the staff member
+     */
     public String getFullName() {
         return this.fullName;
     }
