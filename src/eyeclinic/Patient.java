3f9a87dd7e5ccb8ab5d2a9c22d862bc27b9f097d
Dimitar Mirchev <dirchev@camplight.net>
2016-03-19 17:09:22 +0000

    Add javadoc for Patient class

--- src/eyeclinic/Patient.java
+++ src/eyeclinic/Patient.java
@@ -1,24 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package eyeclinic;
 
 import java.util.ArrayList;
 
 /**
- *
+ * Implementation of Patient interface.
+ * <p>
+ * Holds all fields, needed for a patient in the eye clinic application.
  * @author dirchev
  */
 public class Patient {
-    private String id;
     private String fullName;
     private String phoneNumber;
     private String address;
     private String email;
     private ArrayList<Note> notes;
 
+    /**
+     * Constructs a new patient.
+     * @param fullName the full name of the patient including first, middle and last names
+     * @param phoneNumber the phone number of the patient
+     * @param address the home address of the patient
+     * @param email patient's email
+     */
     public Patient(String fullName, String phoneNumber, String address, String email) {
         this.fullName = fullName;
         this.phoneNumber = phoneNumber;
@@ -27,53 +30,68 @@ public class Patient {
         this.notes = new ArrayList<>();
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
+    /**
+     * Returns the full name of the patient
+     * @return the full name of the patient
+     */
     public String getFullName() {
         return fullName;
     }
 
+    /**
+     * Updates patient's full name
+     * @param fullName updated full name
+     */
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
 
+    /**
+     * Returns the phone number of the patient
+     * @return the phone number of the patient
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     * Updates the phone number of the patient
+     * @param phoneNumber the new phone number
+     */
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
+    /**
+     * Returns the patient's address
+     * @return patient's address
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     * Updates patient's address
+     * @param address the new address to be updated
+     */
     public void setAddress(String address) {
         this.address = address;
     }
 
+    /**
+     * Returns patient's email
+     * @return patient's email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Updates patient's email
+     * @param email the new email to be updated
+     */
     public void setEmail(String email) {
         this.email = email;
     }
-
-    public ArrayList<Note> getNotes() {
-        return notes;
-    }
-
-    public void setNotes(ArrayList<Note> notes) {
-        this.notes = notes;
-    }
-    
     
 }
