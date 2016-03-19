e33ff014145d0e1bdcaab2dd39281f5ec8f37197
Dimitar Mirchev <dirchev@camplight.net>
2016-03-19 17:09:35 +0000

    Add javadoc for Note class

--- src/eyeclinic/Note.java
+++ src/eyeclinic/Note.java
@@ -6,20 +6,47 @@
 package eyeclinic;
 
 /**
- *
+ * Note object. Contains an additional information about eye clinic record. 
+ * <p>
+ * Can be used as a field of different object in the application such as Patient, Appointment or Treatment.
  * @author dirchev
  */
 public class Note {
     private String id;
-    private Staff creator;
+    private final Staff creator;
     private String content;
     
+    /**
+     * Creates a note
+     * @param creator The creator of the note 
+     * @param content Note content
+     */
     public Note (Staff creator, String content) {
         this.creator = creator;
         this.content = content;
     }
+
+    /**
+     * Returns a Staff object of the creator of the note
+     * @return the creator of the note
+     */
+    public Staff getCreator() {
+        return creator;
+    }
     
-    public void updateContent(String content) {
+    /**
+     * Returns a String object with the note content
+     * @return the note content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * Updates the note content
+     * @param content updated note content
+     */
+    public void setContent(String content) {
         this.content = content;
     }
 }
