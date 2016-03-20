fde6d67cb41111c23ce3a57379298034e0bace3e
Dimitar Mirchev <dirchev@camplight.net>
2016-03-20 15:51:38 +0000

    Add documentation for AuthHelper

--- src/eyeclinic/Helpers/AuthHelper.java
+++ src/eyeclinic/Helpers/AuthHelper.java
@@ -9,21 +9,33 @@ import eyeclinic.Main;
 import eyeclinic.Staff;
 
 /**
- *
+ * AuthHelper is used to store the logged in user.
  * @author dirchev
  */
 public class AuthHelper {
     private static Staff loggedInUser;
     
+    /**
+     * Returns the logged in user. If no user is logged in, returns `null`.
+     * @return the logged in user
+     */
     public static Staff getLoggedInUser() {
         return loggedInUser;
     }
 
+    /**
+     * Updates the logged in user. The application scene is changed from login to `app`.
+     * @param loggedInUser Staff object to be saved as logged in user
+     * @throws Exception
+     */
     public static void setLoggedInUser(Staff loggedInUser) throws Exception {
         AuthHelper.loggedInUser = loggedInUser;
         Main.window.setScene(Main.scenes.get("app"));
     }
     
+    /**
+     *
+     */
     public static void logOut() {
         AuthHelper.loggedInUser = null;
         Main.window.setScene(Main.scenes.get("login"));
