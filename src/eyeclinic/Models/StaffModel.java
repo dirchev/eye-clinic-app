e0bb8e3f267581e0a171af20f7f65103f412f5c2
Dimitar Mirchev <dirchev@camplight.net>
2016-03-21 14:15:04 +0000

    Implement load and save data methods for staffModel

--- src/eyeclinic/Models/StaffModel.java
+++ src/eyeclinic/Models/StaffModel.java
@@ -6,7 +6,15 @@
 package eyeclinic.Models;
 
 import eyeclinic.Staff;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.function.Consumer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * StaffModel takes care of storing all staff members in the system
@@ -16,8 +24,7 @@ public class StaffModel {
     private static final ArrayList<Staff> staff = new ArrayList<>();
 
     static {
-        StaffModel.getStaff().add(new Staff("dirchev", "121212", "Dimitar Mirchev", "optician"));
-        StaffModel.getStaff().add(new Staff("john", "121212", "John Cena", "receptionist"));
+        loadData();
     }
     
     /**
@@ -26,6 +33,33 @@ public class StaffModel {
      */
     public static ArrayList<Staff> getStaff() {
         return staff;
+    }
+    
+    public static void saveData () {
+        try (FileOutputStream fs = new FileOutputStream("staff.data")) {
+            ObjectOutputStream os = new ObjectOutputStream(fs);
+            
+            for (Staff member: staff) {
+                os.writeObject(member);
+            }
+            
+            os.close();
+        } catch (IOException ex) {
+            Logger.getLogger(StaffModel.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private static void loadData () {
+        try(FileInputStream fi = new FileInputStream("staff.data"); ObjectInputStream os = new ObjectInputStream(fi)) { 
+            // fi.available() is not the best way to check weather there are more records in the file
+            // however, that is the fastest way to coop with this problem
+            // the better solution is here: http://stackoverflow.com/a/2626193
+            while (fi.available() > 0) {
+                staff.add((Staff)os.readObject());
+            }
+        } catch (IOException | ClassNotFoundException ex) {
+            Logger.getLogger(StaffModel.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     /**
