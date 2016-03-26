b79a27e7419b5eb2823363fdaf2327bb553fa1a1
Dimitar Mirchev <dirchev@camplight.net>
2016-03-26 17:22:41 +0000

    Remove not needed imports

--- src/eyeclinic/UIComponents/PatientItem/PatientItem.java
+++ src/eyeclinic/UIComponents/PatientItem/PatientItem.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package eyeclinic.UIComponents.PatientItem;
 
 import eyeclinic.Pages.PatientPreview.PatientPreview;
@@ -14,8 +9,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 
 /**
  * FXML Controller class
